4c469e5abf6c9a90766a848e396e3aeae7bcff08
katherine <muedasnka@gmail.com>
2019-02-12 16:42:52 -0500

    define routes and creating signin component

--- recorstore-front/src/router/index.js
+++ recorstore-front/src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Signin from '@/components/Signin.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  // let's the url identify with tje route your are going to, if you dont set that you dont see the URL update
+  mode: 'history',
   routes: [
     {
+      path: '/',
+      name: 'Signin',
+      component: Signin
     }
   ]
 })
