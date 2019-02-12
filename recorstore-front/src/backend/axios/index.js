1b9eb7f5575e7031d27b99beb1bac43afe5fb7eb
katherine <muedasnka@gmail.com>
2019-02-12 18:05:57 -0500

    adds record component and adds artist and records to router

--- recorstore-front/src/backend/axios/index.js
+++ recorstore-front/src/backend/axios/index.js
@@ -1,7 +1,5 @@
-// Set to axios some global defaults and assign our API URL as a constant which gets used throughout each request.
 import axios from 'axios'
 
-// find API URL (rails app)
 const API_URL = 'http://localhost:3000'
 
 const securedAxiosInstance = axios.create({
@@ -21,7 +19,6 @@ const plainAxiosInstance = axios.create({
 })
 
 securedAxiosInstance.interceptors.request.use(config => {
-  // check what kind of request it is
   const method = config.method.toUpperCase()
   if (method !== 'OPTIONS' && method !== 'GET') {
     config.headers = {
@@ -29,26 +26,23 @@ securedAxiosInstance.interceptors.request.use(config => {
       'X-CSRF-TOKEN': localStorage.csrf
     }
   }
-  // return config method asuming the method isn't options or get request
   return config
 })
 
 securedAxiosInstance.interceptors.response.use(null, error => {
-  // If a cookie is expired or if there is a 401 response we are going to return a refresh request.
   if (error.response && error.response.config && error.response.status === 401) {
-    // If 401 by expired access cookie, we do a refresh request
     return plainAxiosInstance.post('/refresh', {}, { headers: { 'X-CSRF-TOKEN': localStorage.csrf } })
       .then(response => {
         localStorage.csrf = response.data.csrf
         localStorage.signedIn = true
-        // After another successfull refresh - repeat original request
+
         let retryConfig = error.response.config
         retryConfig.headers['X-CSRF-TOKEN'] = localStorage.csrf
         return plainAxiosInstance.request(retryConfig)
       }).catch(error => {
         delete localStorage.csrf
         delete localStorage.signedIn
-        // redirect to signin if refresh fails
+
         location.replace('/')
         return Promise.reject(error)
       })
